0f15c25476c6d995d0ad6cfdd4eb1d9595d5e9f3
Hagai Helman Tov <hagai.helman@gmail.com>
2019-05-25 18:24:21 +0300

    Prepare for Upload to Github and PyPI
    Add setup.py, setup.cfg and LICENSE.txt.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='meanwhile',
+    version='1.0.0',
+    author='Hagai Helman Tov',
+    author_email='hagai.helman@gmail.com',
+    description='Very easy multithreading',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/hagai-helman/meanwhile',
+    keywords=['threading', 'threads', 'multithreading'],
+    py_modules=['meanwhile'],
+    license='MIT',
+    install_requires=[],
+    python_requires='~=3.6',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        ],
+)
